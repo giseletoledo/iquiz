4d839d59d4ef7b5bba17b8be00862c842815546b
gisele <gisesonia@gmail.com>
2023-07-23 10:16:59 -0300

    Adicionando struct questão e pontuacao

---
+++ iQuiz/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  iQuiz
+//
+//  Created by GISELE TOLEDO on 22/07/23.
+//
+
+import Foundation
--- iQuiz/QuestaoViewController.swift
+++ iQuiz/QuestaoViewController.swift
@@ -8,15 +8,50 @@
 import UIKit
 
 class QuestaoViewController: UIViewController {
+    
+    var pontuacao = 0
+    var numeroQuestao = 0
 
+    @IBOutlet var botoesRespostas: [UIButton]!
+    @IBOutlet weak var tituloQuestaoLabel: UILabel!
+    
+    @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
+        //print(sender.tag)
+        let usuarioAcertouResposta = questoes[numeroQuestao].respostaCorreta == sender.tag
+        
+        if usuarioAcertouResposta {
+            pontuacao += 1
+            print("O usuário acertou!")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        navigationItem
-            .hidesBackButton = true
+        configurarLayout()
+        configurarQuestao()
+        
     }
     
+    func configurarLayout(){
+        navigationItem
+            .hidesBackButton = true
+        tituloQuestaoLabel.numberOfLines = 0
+        tituloQuestaoLabel.textAlignment = .center
+        for botao in botoesRespostas{
+            botao.layer.cornerRadius = 12.0
+        }
+    }
+    
+    func configurarQuestao() {
+        tituloQuestaoLabel.text = questoes[numeroQuestao].titulo
+        
+        for botao in botoesRespostas {
+            let tituloBotao = questoes[numeroQuestao].respostas[botao.tag]
+            botao.setTitle(tituloBotao, for: .normal)
+        }
+                       }
 
     /*
     // MARK: - Navigation
